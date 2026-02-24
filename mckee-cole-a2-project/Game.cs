cc9744ec60907517064c1de68d91557b9afdef18
MckeeCMohawk <000983139@mohawkcollege.ca>
2026-02-24 16:18:12 -0500

    Window Setup

---
+++ mckee-cole-a2-project/Game.cs
@@ -0,0 +1,37 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            // Window Setup
+            Window.SetTitle("Placeholder");
+            Window.SetSize(800, 600);
+            Window.ClearBackground(Color.OffWhite);
+
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+
+        }
+    }
+
+}
